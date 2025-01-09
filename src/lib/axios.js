568c38be7de57c03ec4ceac2ad1530700f1d4277
zzayd30 <zaidlatif30@gmail.com>
2025-01-10 00:37:44 +0500

    Updated for deployment

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.BACKEND_URL + "/api",
+  baseURL: "https://chatease-backend.vercel.app/api",
   withCredentials: true,
 });
--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://chatease-backend.vercel.app";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
