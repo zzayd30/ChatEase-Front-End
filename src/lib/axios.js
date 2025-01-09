0c0cbf13b922726a64a47def091c7096f09dd4f6
zzayd30 <zaidlatif30@gmail.com>
2025-01-10 00:11:12 +0500

    Updated the url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.BACKEND_URL,
+  baseURL: import.meta.env.BACKEND_URL + "/api",
   withCredentials: true,
 });
